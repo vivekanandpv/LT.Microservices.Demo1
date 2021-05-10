d96fcd11917599bdebe0eb2841888c990f695971
vivek <vivek@athenaeum.in>
2021-05-10 15:36:51 +0530

    Add project files.

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.Microservices.Demo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Good afternoon!" });
+        }
+    }
+}
